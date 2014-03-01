76490a9bd2fee4445499ba0d5f4001f2e0088f59
Jayesh Magare <jayesh.magare@gmail.com>
2014-02-28 16:17:26 -0800

    Pushing the plugin to Git
    Pushing the plugin to Git

---
+++ README.md
@@ -0,0 +1,44 @@
+Jquery News Ticker
+====================
+## Demo Link
+	http://technosouls.in/git-projects/jqMenuClick/index.html
+
+## Usage
+
+1. Include jQuery:
+
+	```html
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	```
+
+2. Include plugin's code:
+
+	```html
+	<script src="js/jqMenuClick.js"></script>
+	```
+3. Keep the news data div and list of news in html
+	
+	```html
+	<h3 id="aboutMenuMobile"> About <span class="glyphicon glyphicon-chevron-down" ></span></h5>
+	```
+	
+4. Call the plugin:
+
+	```javascript
+	$(function () {
+		$('#aboutMenuMobile').menuClick({
+			// set values and custom functions
+			menuExpandedClass:"glyphicon glyphicon-chevron-up",
+			menuCollapseClass:"glyphicon glyphicon-chevron-down",
+		});
+	});
+	```
+	
+## Issues:
+	Please file issues on Github : https://github.com/jayeshmagare/jqMenuClick/issues
+	
+## License
+
+	MIT License http://opensource.org/licenses/MIT	
+
+Copyright 2014 Jayesh Magare
---
+++ js/jqMenuClick.js
@@ -0,0 +1,69 @@
+/*
+* menuClick: Jquery plugin for switching data icons attached to navigation's click event
+*
+* latest version and complete README available on Github:
+* https://github.com/jayeshmagare/menuClick
+*
+* Copyright 2013 Jayesh Magare (jayesh.magare@gmail.com)
+* Please file issues on Github : https://github.com/jayeshmagare/menuClick/issues
+* Licensed under the MIT license.
+*/
+
+(function ($) {
+    $.fn.menuClick = function (options) {
+		
+        var options = $.extend({}, $.fn.menuClick.config, options);
+		
+		/* check that the passed element is actually in the DOM */
+		if ($(this).length == 0) {
+			if (window.console && window.console.log) {
+				window.console.log('Element does not exist in DOM!');
+			}
+			else {
+				alert('Element does not exist in DOM!');		
+			}
+			return false;
+		}
+		
+		/* ID of the identifier */
+		var menuId = '#' + $(this).attr('id');
+		
+        return this.each(function () {
+
+			var settings = {				
+				menuExpanded:false
+			};
+			
+			/* Next button click button handler */
+			$(menuId).click(function() {
+				switchIcon();
+			});
+			
+			function switchIcon()
+			{
+				var spanTag = $(menuId).find('span');
+					
+				if(!settings.menuExpanded)
+				{
+					spanTag.removeClass(options.menuCollapseClass);
+					spanTag.addClass(options.menuExpandedClass);
+					settings.menuExpanded = true;
+				}
+				else
+				{
+					spanTag.removeClass(options.menuExpandedClass);
+					spanTag.addClass(options.menuCollapseClass);
+					settings.menuExpanded = false;
+				}
+			}
+			
+        });
+    };
+
+    $.fn.menuClick.config = {
+        // set values and custom functions
+		menuExpandedClass:"fa-main-nav-up",
+		menuCollapseClass:"fa-main-nav-down",
+    };
+
+}(jQuery));
